64be6f374637f9eac3f03f68811c50d45a4c5c26
Filip-Povidernyi <p.poviderniy@gmail.com>
2025-09-28 14:44:06 +0300

    done
    Refactoring on hooks is done

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,45 +1,41 @@
-import React, {Component} from 'react';
+import { useEffect } from 'react';
 import css from './style.module.css';
 
 
 
-class Modal extends Component {
+const Modal = (props) => {
 
 
-    handleClick = (evt) => {
+    const handleClick = (evt) => {
         if (evt.currentTarget === evt.target) {
-            this.props.modalIsClose();
+            props.modalIsClose();
         };
     };
 
-    onPressEsc = (evt) => {
+    const onPressEsc = (evt) => {
         if (evt.code === 'Escape') {
-            this.props.modalIsClose();
+            props.modalIsClose();
         };
     };
 
-    componentDidMount() {
+    useEffect(() => {
         document.body.style.overflow = "hidden";
         document.documentElement.style.overflow = "hidden";
-        window.addEventListener('keyup', this.onPressEsc);
-    };
+        window.addEventListener('keydown', onPressEsc);
+        return () => {
+            document.body.style.overflow = "auto";
+            document.documentElement.style.overflow = "auto";
+            window.removeEventListener('keydown', onPressEsc);
+        }
+    });
 
-    componentWillUnmount() {
-        document.body.style.overflow = "auto";
-        document.documentElement.style.overflow = "auto";
-        window.removeEventListener('keyup', this.onPressEsc);
-    };
-
-    render() {
-    const { bigImage } = this.props;
     return (
-        <div className={css.overlay} onClick={this.handleClick}>
+        <div className={css.overlay} onClick={handleClick}>
             <div className={css.modal}>
-                <img src={bigImage.url} alt={bigImage.tags} />
+                <img src={props.bigImage.url} alt={props.bigImage.tags} />
             </div>
         </div>
     )
-}
 };
 
 
